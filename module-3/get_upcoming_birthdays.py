c661aa07e99cdb6e485e25ee7a5a882b8e4d6d3a
Oleksandr Chaikovskyi <wincchesster@gmail.com>
2024-01-28 20:34:24 +0200

    update get_upcoming_birthdays

--- module-3/get_upcoming_birthdays.py
+++ module-3/get_upcoming_birthdays.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 
 def get_upcoming_birthdays(users):
-    today = datetime.today()
+    today = datetime.today().date()
     upcoming_birthdays = []
 
     for user in users:
@@ -18,22 +18,20 @@ def get_upcoming_birthdays(users):
         delta = (birthday_this_year - today).days
 
         if 0 <= delta <= 7:
-            if birthday_this_year.weekday() in [5, 6]:  # Субота або неділя
+            if birthday_this_year.weekday() in [5, 6]: 
                 offset = 7 - birthday_this_year.weekday()
                 birthday_this_year = birthday_this_year + timedelta(days=offset)
 
-        upcoming_birthdays.append({
+            upcoming_birthdays.append({
                 "name": user["name"],
                 "congratulation_date": birthday_this_year.strftime("%Y.%m.%d")
             })
 
-        return upcoming_birthdays
+    return upcoming_birthdays
 
-
-# Testing the function
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
-    {"name": "Jane Smith", "birthday": "1990.01.27"}
+    {"name": "John Doe", "birthday": "1985.01.29"},
+    {"name": "Jane Smith", "birthday": "1990.02.01"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
